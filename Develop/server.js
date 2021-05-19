c3fb5b14c4249fdc4884b70980e312ae1e0d7c5f
rorylkd <rory.donaldson14@gmail.com>
2021-05-19 08:56:10 +0800

    Added nodemon so I don't have to keep restarting the server, moved my server.js into develop folder. Got the /notes route working

---
+++ Develop/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express()
+
+const port = 9000;
+
+// app.get('*', (request, response) => {
+//     response.sendFile(path.join(__dirname, './public/index.html'));
+// });
+
+app.get('/notes', (request, response) => {
+    response.sendFile(path.join(__dirname, 'public' ,'/notes.html'));
+});
+
+app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`);
+})
