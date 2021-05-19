af260fc4160e37b1323f589d2ff3eebeae51c759
rorylkd <rory.donaldson14@gmail.com>
2021-05-19 08:26:02 +0800

    testing html routes

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -4,6 +4,10 @@ const app = express()
 
 const port = 9000;
 
+app.get('/', (request, response) => {
+    response.send('Test');
+});
+
 app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`);
 })
