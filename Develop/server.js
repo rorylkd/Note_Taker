4e50403a48512f70794ef3f42d2ef96a6c7c7dac
rorylkd <rory.donaldson14@gmail.com>
2021-05-20 13:16:16 +0800

    Started the api/notes route; figured out how to read/parse the json file

--- Develop/server.js
+++ Develop/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 const app = express()
 
@@ -7,6 +8,12 @@ const port = 9000;
 
 app.get('/notes', (request, response) => {
     response.sendFile(path.join(__dirname, 'public' ,'/notes.html'));
+});
+
+app.get('/api/notes', (request, response) => {
+    // Passing require with the path to a JSON file reads and parses the data into an object.
+    const db = require('./db/db.json');
+    console.log(db);
 });
 
 // app.get('*', (request, response) => {
